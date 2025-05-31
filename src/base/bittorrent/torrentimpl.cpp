4db22e94f567336aa99b259582253788f39ae2cb
PartMent <partment@live.com>
2025-05-31 15:46:20 +0800

    feat: Perform like a normal qB

--- src/base/bittorrent/torrentimpl.cpp
+++ src/base/bittorrent/torrentimpl.cpp
@@ -706,11 +706,6 @@ void TorrentImpl::replaceTrackers(QList<TrackerEntry> trackers)
 
     m_nativeHandle.replace_trackers(nativeTrackers);
 
-    // Clear the peer list if it's a private torrent since
-    // we do not want to keep connecting with peers from old tracker.
-    if (isPrivate())
-        clearPeers();
-
     deferredRequestResumeData();
     m_session->handleTorrentTrackersChanged(this);
 }
